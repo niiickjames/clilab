acf8fdbd70346ca1964f94d344177c59aaf5b76e
niiickjames <nickolas.reeves@wiz.io>
2023-03-22 16:06:05 -0500

    2ndcommit

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.12.6"
   
   backend "azurerm" {
-    subscription_id      = "/subscriptions/90ee3a19-1af9-4fe1-a2aa-7f98c1f898e6/resourceGroups/wizDemoResourceGroup/providers/Microsoft.Storage/storageAccounts/pipelinenick21468"
+    subscription_id      = "90ee3a19-1af9-4fe1-a2aa-7f98c1f898e6"
     resource_group_name  = "wizDemoResourceGroup"
     storage_account_name = "pipelinenick21468"
     container_name       = "terraformstate"
